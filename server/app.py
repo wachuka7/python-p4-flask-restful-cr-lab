301f008c80dd2ef3c092fd6b5e36c341eb3b8d9b
wachuka7 <rachaelwachuka7@gmail.com>
2024-04-12 17:32:53 +0300

    feat: add routes

--- server/app.py
+++ server/app.py
@@ -15,13 +15,45 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+class Home(Resource):
+    def get(self):
+        response= make_response(
+            {'message': 'Welcome to Plantsy'},
+            200,
+        )
+        return response
+    
+api.add_resource(Home, '/')
+
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            plants,
+            200
+        )
+        return response
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(name=data['name'], image=data['image'], price=data['price'])
+        db.session.add(new_plant)
+        db.session.commit()
+        response= make_response(
+            new_plant.to_dict(),
+            201,
+        )
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get_or_404(id)
+        return jsonify(plant.to_dict())
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.DECIMAL(precision=10, scale=2), nullable=False)
+
+    def __repr__(self):
+        return f'<Plant: {self.name}, Price: {self.price}'
